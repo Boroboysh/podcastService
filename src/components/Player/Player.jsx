3c953a6e23877af8d8e7e46bca8375ac2b7b745c
Artyom Klim <artyomklim001@gmail.com>
2022-07-01 16:00:28 +0300

    Project initialization

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './player.module.css'
+
+function Player () {
+    return (
+        <div className={styles.wrap}>
+
+        </div>
+    )
+}
+
+export default Player
